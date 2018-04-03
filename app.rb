5d97f082aaf7fdbe0a99fe154e1f7c7e52be6bcd
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-03 17:17:15 +0100

    got name working and produced form, not fully implemented.

---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+
+get '/' do
+    "Hello World"
+end
+
+get '/secret' do
+    "My little secret..."
+end
+
+get '/jack' do
+    "Using shotgun!"
+end
+
+get '/random-cat' do
+    @name = ["Amigo", "Oscar", "Viking"].sample
+    erb(:index)
+end
+
+get '/named-cat' do
+    if !params.empty? then
+        @fname = params[:name] + ' '
+        @sname = params[:surname]
+        p params
+    end
+    erb(:index)
+end
